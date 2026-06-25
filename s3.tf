0a9d585f1a99912361889d86d6fc3be5f4ccd43a
LironeFitoussi <lironefit@gmail.com>
2026-06-25 10:46:16 +0300

    Add initial Terraform configuration for S3 static website deployment
    - Create .gitignore to exclude sensitive and state files
    - Add index.html and error.html for website content
    - Define S3 bucket and website configuration in s3.tf

---
+++ s3.tf
@@ -0,0 +1,56 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+  # random_id.bucket_suffix.hex = # random_id.bucket_suffix.hex
+}
+
+resource "aws_s3_bucket" "static_website" {
+  bucket = "terraform-course-project-${random_id.bucket_suffix.hex}"
+
+  force_destroy = true
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_website_public_read" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "PublicReadObjects",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+output "production_url" {
+  value = "Your website is available at: http://${aws_s3_bucket.static_website.website_endpoint}"
+}
